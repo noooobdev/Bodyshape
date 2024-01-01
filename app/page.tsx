93ddbbce4b9166b9d7894dc82fcdd3e51ee96f21
noooobdev <nirajbohara10@gmail.com>
2024-01-01 21:13:59 +0545

    GG

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import Navbar from "./Components/Navbar";
+import top from "./Assets/top-mountain.png";
 
 export default function Home() {
   return (
-    <div className="font-roboto">
+    <div className="h-screen w-screen">
+      <div>
+        <Image src={top} alt="Mountain" layout="screen" objectFit="cover" />
+      </div>
       <Navbar />
     </div>
   );
