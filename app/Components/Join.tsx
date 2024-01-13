96211a4ecd32ab20d691be2a03bdfeafd6349693
noooobdev <nirajbohara10@gmail.com>
2024-01-13 20:41:07 +0545

    init Join us

--- app/Components/Join.tsx
+++ app/Components/Join.tsx
@@ -3,9 +3,19 @@ import React from "react";
 const Join = () => {
   return (
     <div>
-      <h1 className="text-5xl font-semibold text-white px-16 py-16 ">
-        Why Join With <span className="text-[#FF921B]">Us?</span>
-      </h1>
+      <div className="px-16 py-16">
+        <h1 className="text-5xl font-semibold text-white   ">
+          Why Join With <span className="text-[#FF921B]">Us?</span>
+        </h1>
+        <div className="bg-gradient-to-b from-[#1d1d1d] to-black w-1/3">
+          <ol className="text-[#ff921b] pl-20">
+            <li className="text-white">Best Gym</li>
+            <li className="text-white">Expert Coach</li>
+            <li className="text-white">Good Workout Facilities</li>
+            <li className="">Consulation With Experts</li>
+          </ol>
+        </div>
+      </div>
     </div>
   );
 };
