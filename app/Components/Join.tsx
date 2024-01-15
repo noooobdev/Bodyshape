cc407d086e456e23f4e4f95fbb92bfa3457be479
noooobdev <nirajbohara10@gmail.com>
2024-01-15 20:28:18 +0545

    style join section

--- app/Components/Join.tsx
+++ app/Components/Join.tsx
@@ -3,18 +3,20 @@ import React from "react";
 const Join = () => {
   return (
     <div className="px-20 py-5">
-      <div className="flex justify-between gap-60 items-start w-full">
+      <div className="flex justify-between items-start w-full">
         <div className="flex flex-col gap-10 w-1/2">
           <h1 className="text-5xl font-semibold text-white  ">
             Why Join With <span className="text-[#FF921B]">Us?</span>
           </h1>
           <div className="pl-20">
-            <div className="bg-gradient-to-b from-[#1d1d1d] to-black ">
-              <ol className="text-[#ff921b]  p-10">
-                <li className="text-white">Best Gym</li>
-                <li className="text-white">Expert Coach</li>
-                <li className="text-white">Good Workout Facilities</li>
-                <li className="">Consulation With Experts</li>
+            <div className="bg-gradient-to-b from-[#080808] to-black ">
+              <ol className="marker:text-[#ff921b] marker:text-xl list-disc p-10">
+                <div className="text-white text-xl font-medium">
+                  <li>Best Gym</li>
+                  <li>Expert Coach</li>
+                  <li>Good Workout Facilities</li>
+                  <li>Consulation With Experts</li>
+                </div>
               </ol>
             </div>
             <button className="bg-gradient-to-r from-orange-600 to-amber-500 p-2 px-4 text-white">
@@ -27,15 +29,15 @@ const Join = () => {
           <div>
             <img
               src="https://th.bing.com/th/id/OIP.tP1U9qP_z1OwpsJdlaqnEgHaHa?rs=1&pid=ImgDetMain"
-              alt="lol"
-              className="w-52 h-52"
+              alt="running"
+              className="w-52 h-52 object-cover"
             />
           </div>
           <div className="pl-20">
             <img
-              src="https://th.bing.com/th/id/OIP.tP1U9qP_z1OwpsJdlaqnEgHaHa?rs=1&pid=ImgDetMain"
-              alt="lol"
-              className="w-52 h-52 "
+              src="https://cdn.shopify.com/s/files/1/1153/7200/files/The_Mental_Benefits_of_Resistance_Training-squashed.jpg?v=1611328895"
+              alt="dumbell-lifting"
+              className="w-52 h-52 object-cover"
             />
           </div>
         </div>
